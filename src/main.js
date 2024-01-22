8abfa1c182e897b0ec327a8fb20598cf0a3196dd
Gyungmin Roh <gyroh@ucsc.edu>
2024-01-22 13:03:17 -0800

    new file:   assets/.DS_Store 	new file:   assets/explosion.png 	new file:   assets/rocket.png 	new file:   assets/sfx-explosion.wav 	new file:   assets/sfx-select.wav 	new file:   assets/sfx-shot.wav 	new file:   assets/spaceship.png 	new file:   assets/starfield.png 	modified:   index.html 	modified:   src/.DS_Store 	modified:   src/main.js 	new file:   src/prefabs/Rocket.js 	new file:   src/prefabs/Spaceship.js 	modified:   src/scenes/Menu.js 	modified:   src/scenes/Play.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,15 @@ let config = {
     type: Phaser.AUTO,
     width: 640,
     height: 480,
-    scene: [ ]
+    scene: [ Menu, Play ]
 }
 
 let game = new Phaser.Game(config)
+
+// reserve keyboard bindings
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
+
+// SET US constructions
+let borderUISize = game.config.height / 15
+let borderPadding = borderUISize / 3
+
